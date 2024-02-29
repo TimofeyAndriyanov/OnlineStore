8ed3a6e27cfa9f5b8b5fe5693fee61e46497e5e4
TimofeyAndriyanov <158852595+TimofeyAndriyanov@users.noreply.github.com>
2024-02-29 05:25:19 +0300

    Добавление базового проекта

---
+++ app/src/main/java/ru/onlinestore/mobile/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.onlinestore.mobile.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/ru/onlinestore/mobile/ui/theme/Theme.kt
@@ -0,0 +1,2 @@
+package ru.onlinestore.mobile.ui.theme
+
