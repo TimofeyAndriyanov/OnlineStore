9266747b2c86bed9a660e8d7f27bafddda7a40c3
TimofeyAndriyanov <158852595+TimofeyAndriyanov@users.noreply.github.com>
2024-03-01 16:29:49 +0300

    добавление usecase-ов

---
+++ app/src/main/java/ru/onlinestore/mobile/domain/usecase/GetListProductsUseCase.kt
@@ -0,0 +1,46 @@
+package ru.onlinestore.mobile.domain.usecase
+
+import ru.onlinestore.mobile.domain.repository.ProductRepository
+import ru.onlinestore.mobile.domain.models.ProductListDomain
+import ru.onlinestore.mobile.domain.models.ProductDomain
+import ru.onlinestore.mobile.ui.models.ProductListUi
+import ru.onlinestore.mobile.core.ResultContainer
+import ru.onlinestore.mobile.ui.models.ProductUi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetListProductsUseCase(private val productRepository: ProductRepository) {
+
+    operator fun invoke(): Flow<ResultContainer<ProductListUi>> = flow {
+        emit(productRepository.fetchListProducts().map(mapper))
+    }
+
+    private val mapper = { products: ProductListDomain ->
+        ProductListUi(
+            products = mapperProductUi(products.products),
+            total = products.total,
+            skip = products.skip,
+            limit = products.limit
+        )
+    }
+
+    private val mapperProductUi = { products: List<ProductDomain> ->
+        products.map { product ->
+            ProductUi(
+                id = product.id,
+                title = product.title,
+                description = product.description,
+                price = product.price,
+                discountPercentage = product.discountPercentage,
+                rating = product.rating,
+                stock = product.stock,
+                brand = product.brand,
+                category = product.category,
+                thumbnail = product.thumbnail,
+                images = product.images
+            )
+        }
+
+    }
+
+}
---
+++ app/src/main/java/ru/onlinestore/mobile/domain/usecase/GetProductByIdUseCase.kt
@@ -0,0 +1,10 @@
+package ru.onlinestore.mobile.domain.usecase
+
+import ru.onlinestore.mobile.domain.models.ProductDomain
+
+class GetProductByIdUseCase {
+
+    operator fun invoke(vararg id: Long): List<ProductDomain> =
+        TODO("Пока реализации usecase нету")
+
+}
